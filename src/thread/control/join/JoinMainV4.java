e68087a7a2f996b48968aef600ec12bc3d2d2750
woogie <woogisfree@gmail.com>
2024-12-01 16:38:53 +0900

    feat: Thread join(), join(ms)

---
+++ src/thread/control/join/JoinMainV4.java
@@ -0,0 +1,44 @@
+package thread.control.join;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class JoinMainV4 {
+
+    public static void main(String[] args) throws InterruptedException {
+        log("main Start");
+        SumTask task1 = new SumTask(1, 50);
+        Thread thread1 = new Thread(task1, "thread-1");
+        thread1.start();
+
+        /**
+         * 1초 대기, main Thread Status : TIMED_WAITING
+         */
+        log("join(1000) - main 스레드가 thread1이 종료될 때까지 1초 대기");
+        thread1.join(1000);
+        log("join(1000) - main 스레드 대기 완료");
+
+        log("task1.result =" + task1.result);
+    }
+
+    static class SumTask implements Runnable {
+        int startValue;
+        int endValue;
+        int result = 0;
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+        @Override
+        public void run() {
+            log("작업 시작");
+            sleep(2000);
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+            log("작업 완료 result = " + result);
+        }
+    }
+}
