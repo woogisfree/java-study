1ea24c2894ce4717a12340825746570e7c14b4c2
woogie <woogisfree@gmail.com>
2024-12-03 16:42:19 +0900

    feat: concurrent.locks.Lock, ReentranceLock

--- src/thread/sync/BankAccountV4.java
+++ src/thread/sync/BankAccountV4.java
@@ -1,15 +1,19 @@
 package thread.sync;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
 /**
- * synchronized 메소드를 synchronized 블록으로 변경
+ * Lock interface 사용
  */
-public class BankAccountV3 implements BankAccount {
+public class BankAccountV4 implements BankAccount {
     private int balance;
+    private final Lock lock = new ReentrantLock();
 
-    public BankAccountV3(int initialBalance) {
+    public BankAccountV4(int initialBalance) {
         this.balance = initialBalance;
     }
 
@@ -17,7 +21,8 @@ public class BankAccountV3 implements BankAccount {
     public boolean withdraw(int amount) {
         log("거래 시작: " + getClass().getSimpleName());
 
-        synchronized (this) {
+        lock.lock();
+        try {
             log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
             if (balance < amount) {
                 log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
@@ -27,14 +32,20 @@ public class BankAccountV3 implements BankAccount {
             sleep(1000);
             balance -= amount;
             log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + balance);
+        } finally {
+            lock.unlock();
         }
-
         log("거래 종료");
         return true;
     }
 
     @Override
-    public synchronized int getBalance() {
-        return balance;
+    public int getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
     }
 }
--- src/thread/sync/BankAccountV5.java
+++ src/thread/sync/BankAccountV5.java
@@ -1,15 +1,19 @@
 package thread.sync;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 import static util.MyLogger.log;
 import static util.ThreadUtils.sleep;
 
 /**
- * synchronized 메소드를 synchronized 블록으로 변경
+ * tryLock()
  */
-public class BankAccountV3 implements BankAccount {
+public class BankAccountV5 implements BankAccount {
     private int balance;
+    private final Lock lock = new ReentrantLock();
 
-    public BankAccountV3(int initialBalance) {
+    public BankAccountV5(int initialBalance) {
         this.balance = initialBalance;
     }
 
@@ -17,24 +21,33 @@ public class BankAccountV3 implements BankAccount {
     public boolean withdraw(int amount) {
         log("거래 시작: " + getClass().getSimpleName());
 
-        synchronized (this) {
+        if (!lock.tryLock()) {
+            log("[진입 실패] 이미 처리중인 작업이 있습니다.");
+            return false;
+        }
+        try {
             log("[검증 시작] 출금액: " + amount + ", 잔액: " + balance);
             if (balance < amount) {
                 log("[검증 실패] 출금액: " + amount + ", 잔액: " + balance);
                 return false;
             }
-            log("[검증 완료] 출금액: " + amount + ", 잔액: " + balance);
             sleep(1000);
             balance -= amount;
             log("[출금 완료] 출금액: " + amount + ", 변경 잔액: " + balance);
+        } finally {
+            lock.unlock();
         }
-
         log("거래 종료");
         return true;
     }
 
     @Override
-    public synchronized int getBalance() {
-        return balance;
+    public int getBalance() {
+        lock.lock();
+        try {
+            return balance;
+        } finally {
+            lock.unlock();
+        }
     }
 }
