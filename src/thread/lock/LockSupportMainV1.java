3c5eea04e64f38bd88597733ce89543bfd353b2b
woogie <woogisfree@gmail.com>
2024-12-03 16:05:15 +0900

    feat: LockSupport park, unpark

---
+++ src/thread/lock/LockSupportMainV1.java
@@ -0,0 +1,32 @@
+package thread.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportMainV1 {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ParkTask(), "Thread-1");
+        thread1.start();
+
+        sleep(100);
+        log("Thread-1 state: " + thread1.getState());
+
+        log("main -> unpark(Thread-1)");
+//        LockSupport.unpark(thread1);
+        thread1.interrupt();
+    }
+
+    static class ParkTask implements Runnable {
+
+        @Override
+        public void run() {
+            log("park 시작");
+            LockSupport.park();
+            log("park 종료, state: " + Thread.currentThread().getState());
+            log("인터럽트 상태: " + Thread.currentThread().isInterrupted());
+        }
+    }
+}
---
+++ src/thread/lock/LockSupportMainV2.java
@@ -0,0 +1,28 @@
+package thread.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportMainV2 {
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new ParkTask(), "Thread-1");
+        thread1.start();
+
+        sleep(100);
+        log("Thread-1 state: " + thread1.getState());
+    }
+
+    static class ParkTask implements Runnable {
+
+        @Override
+        public void run() {
+            log("park 시작, 2초 대기");
+            LockSupport.parkNanos(2000_000000);
+            log("park 종료, state: " + Thread.currentThread().getState());
+            log("인터럽트 상태: " + Thread.currentThread().isInterrupted());
+        }
+    }
+}
