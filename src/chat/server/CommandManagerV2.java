2624410b1eef9d1c8477aeab4502d0bb74fcd4f3
woogie <woogisfree@gmail.com>
2024-12-23 15:44:15 +0900

    feat: 채팅 프로그램 - version2

---
+++ src/chat/server/CommandManagerV2.java
@@ -0,0 +1,48 @@
+package chat.server;
+
+import java.io.IOException;
+import java.util.List;
+
+import static util.MyLogger.log;
+
+public class CommandManagerV2 implements CommandManager {
+
+    public static final String DELIMITER = "\\|";
+    private final SessionManager sessionManager;
+
+    public CommandManagerV2(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Override
+    public void execute(String totalMessage, Session session) throws IOException {
+        log("CommandManagerV2: " + totalMessage);
+        if (totalMessage.startsWith("/join")) {
+            String[] split = totalMessage.split(DELIMITER);
+            String username = split[1];
+            session.setUsername(username);
+            sessionManager.sendAll(username + "님이 입장했습니다.");
+        } else if (totalMessage.startsWith("/message")) {
+            String[] split = totalMessage.split(DELIMITER);
+            String message = split[1];
+            sessionManager.sendAll("[" + session.getUsername() + "] " + message);
+        } else if (totalMessage.startsWith("/change")) {
+            String[] split = totalMessage.split(DELIMITER);
+            String changeName = split[1];
+            sessionManager.sendAll(session.getUsername() + "님이 " + changeName + "로 이름을 변경했습니다.");
+            session.setUsername(changeName);
+        } else if (totalMessage.startsWith("/users")) {
+            List<String> usernames = sessionManager.getAllUsername();
+            StringBuilder sb = new StringBuilder();
+            sb.append("전체 접속자 : ").append(usernames.size()).append("\n");
+            for (String username : usernames) {
+                sb.append(" - ").append(username).append("\n");
+            }
+            session.send(sb.toString());
+        } else if (totalMessage.startsWith("/exit")) {
+            throw new IOException("exit");
+        } else {
+            session.send("처리할 수 없는 명령어입니다: " + totalMessage);
+        }
+    }
+}
