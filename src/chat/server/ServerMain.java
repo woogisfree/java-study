348238579ca32a2099192807b012bffcebcef71e
woogie <woogisfree@gmail.com>
2024-12-23 15:58:23 +0900

    feat: 채팅 프로그램 - version3 (Command Pattern)

--- src/chat/server/ServerMain.java
+++ src/chat/server/ServerMain.java
@@ -10,7 +10,9 @@ public class ServerMain {
         SessionManager sessionManager = new SessionManager();
 
 //        CommandManager commandManager = new CommandManagerV1(sessionManager);
-        CommandManager commandManager = new CommandManagerV2(sessionManager);
+//        CommandManager commandManager = new CommandManagerV2(sessionManager);
+//        CommandManager commandManager = new CommandManagerV3(sessionManager);
+        CommandManager commandManager = new CommandManagerV4(sessionManager);
 
         Server server = new Server(PORT, commandManager, sessionManager);
         server.start();
