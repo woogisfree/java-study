0b3e1ad381474b33a66f74fb28fcc683af40a7a9
woogie <woogisfree@gmail.com>
2024-12-24 19:04:58 +0900

    feat: HTTP 서버 만들기1

---
+++ src/was/v1/HttpServerV1.java
@@ -0,0 +1,24 @@
+package was.v1;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static util.MyLogger.log;
+
+public class HttpServerV1 {
+
+    private final int port;
+
+    public HttpServerV1(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        log("서버 시작 port: " + port);
+        while (true) {
+            Socket socket = serverSocket.accept();
+        }
+    }
+}
