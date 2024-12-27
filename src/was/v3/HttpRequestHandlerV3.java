59f215ba33216290f6d6d45df28cdbba8ffecb8e
woogie <woogisfree@gmail.com>
2024-12-27 18:30:37 +0900

    feat: HTTP 서버 만들기 - 기능 추가1

---
+++ src/was/v3/HttpRequestHandlerV3.java
@@ -0,0 +1,71 @@
+package was.v3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static util.MyLogger.log;
+
+public class HttpRequestHandlerV3 implements Runnable {
+
+    private final Socket socket;
+
+    public HttpRequestHandlerV3(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            process(socket);
+        } catch (Exception e) {
+            log(e);
+        }
+    }
+
+    private void process(Socket socket) throws IOException {
+        try (socket;
+             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), UTF_8));
+             PrintWriter writer = new PrintWriter(socket.getOutputStream(), false, UTF_8)) {
+
+            String requestString = requestToString(reader);
+            if (requestString.contains("/favicon.ico")) {
+                log("favicon 요청");
+                return;
+            }
+            log("HTTP 요청 정보 출력");
+            System.out.println(requestString);
+
+            log("HTTP 응답 생성중...");
+            if (requestString.startsWith("GET /site1")) {
+                site1(writer);
+            } else if (requestString.startsWith("GET /site2")) {
+                site2(writer);
+            } else if (requestString.startsWith("GET /search")) {
+                search(writer, requestString);
+            } else if (requestString.startsWith("GET / ")) {
+                home(writer);
+            } else {
+                notFound(writer);
+            }
+            log("HTTP 응답 전달 완료");
+        }
+    }
+
+    private String requestToString(BufferedReader reader) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            if (line.isEmpty()) break;
+            sb.append(line).append("\n");
+        }
+        return sb.toString();
+    }
+
+    private static void home(PrintWriter writer) {
+        writer.println();
+    }
+}
