5227a820ac8a949380ddeb0b3966f9bc1089516b
woogie <woogisfree@gmail.com>
2025-01-11 01:53:37 +0900

    feat: http server - annotation servlet - 동적 바인딩

---
+++ src/was/httpserver/servlet/annotation/AnnotationServletV2.java
@@ -0,0 +1,59 @@
+package was.httpserver.servlet.annotation;
+
+import was.httpserver.HttpRequest;
+import was.httpserver.HttpResponse;
+import was.httpserver.HttpServlet;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class AnnotationServletV2 implements HttpServlet {
+
+    private final List<Object> controllers;
+
+    public AnnotationServletV2(List<Object> controllers) {
+        this.controllers = controllers;
+    }
+
+    @Override
+    public void service(HttpRequest request, HttpResponse response) throws IOException {
+        String path = request.getPath();
+
+        for (Object controller : controllers) {
+            Method[] methods = controller.getClass().getDeclaredMethods();
+            for (Method method : methods) {
+                if (method.isAnnotationPresent(Mapping.class)) {
+                    Mapping mapping = method.getAnnotation(Mapping.class);
+                    String value = mapping.value();
+                    if (value.equals(path)) {
+                        invoke(request, response, controller, method);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    private void invoke(HttpRequest request, HttpResponse response, Object controller, Method method) {
+        Class<?>[] parameterTypes = method.getParameterTypes();
+        Object[] args = new Object[parameterTypes.length];
+
+        for (int i = 0; i < parameterTypes.length; i++) {
+            if (parameterTypes[i] == HttpRequest.class) {
+                args[i] = request;
+            } else if (parameterTypes[i] == HttpResponse.class) {
+                args[i] = response;
+            } else {
+                throw new IllegalArgumentException("Unsupported parameter type: " + parameterTypes[i]);
+            }
+        }
+
+        try {
+            method.invoke(controller, args);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/was/v8/ServerMainV8.java
@@ -0,0 +1,26 @@
+package was.v8;
+
+import was.httpserver.HttpServer;
+import was.httpserver.HttpServlet;
+import was.httpserver.ServletManager;
+import was.httpserver.servlet.annotation.AnnotationServletV2;
+import was.v5.servlet.DiscardServlet;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ServerMainV8 {
+
+    private static final int PORT = 12345;
+
+    public static void main(String[] args) throws IOException {
+        List<Object> controllers = List.of(new SiteControllerV8(), new SearchControllerV8());
+        HttpServlet servlet = new AnnotationServletV2(controllers);
+
+        ServletManager servletManager = new ServletManager();
+        servletManager.setDefaultServlet(servlet);
+        servletManager.add("/favicon.ico", new DiscardServlet());
+        HttpServer server = new HttpServer(PORT, servletManager);
+        server.start();
+    }
+}
