26d581dc486f8f0c9bfcd9e876387ac5757b97cb
woogie <woogisfree@gmail.com>
2025-01-28 13:12:52 +0900

    feat: 절차 지향 프로그래밍4

--- src/basics/oop1/ValueObjectMain.java
+++ src/basics/oop1/ValueObjectMain.java
@@ -2,12 +2,12 @@ package basics.oop1;
 
 public class ValueObjectMain {
 
-
     public static void main(String[] args) {
         ValueObject valueObject = new ValueObject();
         valueObject.add();
         valueObject.add();
         valueObject.add();
+
         System.out.println("최종 숫자=" + valueObject.value);
     }
 }
