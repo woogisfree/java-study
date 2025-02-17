af172502f3899f7aa57845ac8d3c9761ecedfb43
woogie <woogisfree@gmail.com>
2025-02-18 00:49:36 +0900

    feat: 상속 관계

---
+++ src/basics/extends1/ex2/Car.java
@@ -0,0 +1,8 @@
+package basics.extends1.ex2;
+
+public class Car {
+
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+}
---
+++ src/basics/extends1/ex2/ElectricCar.java
@@ -0,0 +1,8 @@
+package basics.extends1.ex2;
+
+public class ElectricCar extends Car {
+
+    public void charge() {
+        System.out.println("충전합니다.");
+    }
+}
