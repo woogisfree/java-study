bc52e13e904e5aab4024fa6a2b443b48eddd1251
woogie <woogisfree@gmail.com>
2025-02-21 23:43:03 +0900

    feat: 상속과 접근 제어

---
+++ src/basics/extends1/access/ExtendsAccessMain.java
@@ -0,0 +1,11 @@
+package basics.extends1.access;
+
+import basics.extends1.access.child.Child;
+
+public class ExtendsAccessMain {
+
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
