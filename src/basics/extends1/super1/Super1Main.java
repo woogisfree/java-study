2f9a23b024b47400b1b47a6932541af03967d046
woogie <woogisfree@gmail.com>
2025-02-22 22:15:43 +0900

    feat: super - 부모 참조

---
+++ src/basics/extends1/super1/Super1Main.java
@@ -0,0 +1,10 @@
+package basics.extends1.super1;
+
+import basics.extends1.access.child.Child;
+
+public class Super1Main {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.call();
+    }
+}
