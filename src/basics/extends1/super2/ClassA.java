42005590513f4dfab10a1de3d8e3e70d30ab822c
woogie <woogisfree@gmail.com>
2025-02-23 23:20:39 +0900

    feat: super - 생성자

---
+++ src/basics/extends1/super2/ClassA.java
@@ -0,0 +1,8 @@
+package basics.extends1.super2;
+
+public class ClassA {
+
+    public ClassA() {
+        System.out.println("ClassA 생성자");
+    }
+}
---
+++ src/basics/extends1/super2/Super2Main.java
@@ -0,0 +1,8 @@
+package basics.extends1.super2;
+
+public class Super2Main {
+
+    public static void main(String[] args) {
+        ClassC classC = new ClassC();
+    }
+}
