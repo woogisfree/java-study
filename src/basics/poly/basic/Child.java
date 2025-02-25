439607fba3bb4f40f3dad242b05b4a04d4a94f61
woogie <woogisfree@gmail.com>
2025-02-25 22:07:26 +0900

    feat: 다형성 시작

---
+++ src/basics/poly/basic/Child.java
@@ -0,0 +1,9 @@
+package basics.poly.basic;
+
+public class Child extends Parent {
+
+    public void childMethod() {
+        System.out.println("Child.childMethod");
+    }
+}
+
---
+++ src/basics/poly/basic/PolyMain.java
@@ -0,0 +1,20 @@
+package basics.poly.basic;
+
+public class PolyMain {
+    public static void main(String[] args) {
+        System.out.println("Parent -> Parent");
+        Parent parent = new Parent();
+        parent.parentMethod();
+
+        System.out.println("Child -> Child");
+        Child child = new Child();
+        child.parentMethod();
+        child.childMethod();
+
+        System.out.println("Parent -> Child");
+        Parent poly = new Child();
+
+        poly.parentMethod();
+        //poly.childMethod();
+    }
+}
