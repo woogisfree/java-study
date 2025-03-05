6ac50c9bf230404e698370ccf1f1ddb15dd1b301
woogie <woogisfree@gmail.com>
2025-03-06 00:56:55 +0900

    feat: 추상 클래스2

---
+++ src/basics/poly/ex4/Caw.java
@@ -0,0 +1,14 @@
+package basics.poly.ex4;
+
+public class Caw extends AbstractAnimal {
+
+    @Override
+    public void sound() {
+        System.out.println("음매");
+    }
+
+    @Override
+    public void move() {
+        System.out.println("소 이동");
+    }
+}
