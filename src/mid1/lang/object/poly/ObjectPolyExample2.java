970d3671e30ae8f63ee71b6b2fd8c461ab948495
woogie <woogisfree@gmail.com>
2025-03-17 23:44:34 +0900

    feat: Object 배열

---
+++ src/mid1/lang/object/poly/ObjectPolyExample2.java
@@ -0,0 +1,17 @@
+package mid1.lang.object.poly;
+
+public class ObjectPolyExample2 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Car car = new Car();
+        Object object = new Object();
+
+        Object[] objects = {dog, car, object};
+        size(objects);
+    }
+
+    private static void size(Object[] objects) {
+        System.out.println("Size of objects: " + objects.length);
+    }
+}
