a9d028d58fdb33ce0429e69f1762d06fa9a14456
woogie <woogisfree@gmail.com>
2025-03-26 23:01:56 +0900

    feat: 불변 객체 값 변경

---
+++ src/mid1/lang/immutable/change/MutableMain.java
@@ -0,0 +1,11 @@
+package mid1.lang.immutable.change;
+
+public class MutableMain {
+
+    public static void main(String[] args) {
+        MutableObj obj = new MutableObj(10);
+        obj.add(20);
+
+        System.out.println("obj.getValue() = " + obj.getValue());
+    }
+}
