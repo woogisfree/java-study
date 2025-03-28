490271be9d4e562397cacbb3974a2f969ff2d8e8
woogie <woogisfree@gmail.com>
2025-03-28 22:51:51 +0900

    feat: String 클래스

---
+++ src/mid1/lang/string/CharArrayMain.java
@@ -0,0 +1,12 @@
+package mid1.lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+        char[] charArr = {'h', 'e', 'l', 'l', 'o'};
+        System.out.println(charArr);
+
+        String str = "hello";
+        System.out.println("str = " + str);
+    }
+}
