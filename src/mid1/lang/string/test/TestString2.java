7377968352c28ec268878564cf692a6dc35af44c
woogie <woogisfree@gmail.com>
2025-04-14 23:49:38 +0900

    feat: String 메서드 예제1

---
+++ src/mid1/lang/string/test/TestString2.java
@@ -0,0 +1,13 @@
+package mid1.lang.string.test;
+
+public class TestString2 {
+
+    public static void main(String[] args) {
+        String[] arr = {"hello", "java", "jvm", "spring", "jpa"};
+        int sum = 0;
+        for (String s : arr) {
+            sum += s.length();
+        }
+        System.out.println("sum = " + sum);
+    }
+}
